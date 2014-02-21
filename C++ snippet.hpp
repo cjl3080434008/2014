543ef7196c719b67fdd2ee4e684b7f32e50f87fd
jianlin chen <154913283@qq.com>
2014-02-21 11:18:40 +0800

    new books

--- C++ snippet.hpp
+++ C++ snippet.hpp
@@ -3,4 +3,22 @@
 	template <> _Ty* Singleton<_Ty>::_instance = NULL;   // 特化版本的宏
 
 2.
-
+struct rwlock
+{
+	int read;
+	int write;
+};
+static inline void
+rwlock_rlock(struct rwlock *lock) {
+	for (;;) {
+		while(lock->write) {
+			__sync_synchronize();
+		}
+		__sync_add_and_fetch(&lock->read,1);
+		if (lock->write) {		// 这里是一个比较忽略的点，在给lock->read加一之前已经提前进入write。
+			__sync_sub_and_fetch(&lock->read,1);
+		} else {
+			break;
+		}
+	}
+}
