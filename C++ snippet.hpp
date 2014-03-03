197e951a9f4040a0e17ac62ea96b014078a80eab
jianlin chen <154913283@qq.com>
2014-03-03 18:13:10 +0800

    xxxxxxxxxxx

--- C++ snippet.hpp
+++ C++ snippet.hpp
@@ -22,3 +22,98 @@ rwlock_rlock(struct rwlock *lock) {
 		}
 	}
 }
+
+3. #define DISABLE_COPY_AND_ASSIGN(TypeName)\
+		TypeName(const TypeName &);			\
+		const TypeName &operator=(const TypeName &)
+
+4.union {
+
+            //  Sent to I/O thread to let it know that it should
+            //  terminate itself.
+            struct {
+            } stop;
+
+            //  Sent to I/O object to make it register with its I/O thread.
+            struct {
+            } plug;
+
+            //  Sent to socket to let it know about the newly created object.
+            struct {
+                zmq::own_t *object;
+            } own;
+
+            //  Attach the engine to the session. If engine is NULL, it informs
+            //  session that the connection have failed.
+            struct {
+                struct i_engine *engine;
+            } attach;
+
+            //  Sent from session to socket to establish pipe(s) between them.
+            //  Caller have used inc_seqnum beforehand sending the command.
+            struct {
+                zmq::pipe_t *pipe;
+            } bind;
+
+            //  Sent by pipe writer to inform dormant pipe reader that there
+            //  are messages in the pipe.
+            struct {
+            } activate_read;
+
+            //  Sent by pipe reader to inform pipe writer about how many
+            //  messages it has read so far.
+            struct {
+                uint64_t msgs_read;
+            } activate_write;
+
+            //  Sent by pipe reader to writer after creating a new inpipe.
+            //  The parameter is actually of type pipe_t::upipe_t, however,
+            //  its definition is private so we'll have to do with void*.
+            struct {
+                void *pipe;
+            } hiccup;
+
+            //  Sent by pipe reader to pipe writer to ask it to terminate
+            //  its end of the pipe.
+            struct {
+            } pipe_term;
+
+            //  Pipe writer acknowledges pipe_term command.
+            struct {
+            } pipe_term_ack;
+
+            //  Sent by I/O object ot the socket to request the shutdown of
+            //  the I/O object.
+            struct {
+                zmq::own_t *object;
+            } term_req;
+
+            //  Sent by socket to I/O object to start its shutdown.
+            struct {
+                int linger;
+            } term;
+
+            //  Sent by I/O object to the socket to acknowledge it has
+            //  shut down.
+            struct {
+            } term_ack;
+
+            //  Transfers the ownership of the closed socket
+            //  to the reaper thread.
+            struct {
+                zmq::socket_base_t *socket;
+            } reap;
+
+            //  Closed socket notifies the reaper that it's already deallocated.
+            struct {
+            } reaped;
+
+            //  Sent by reaper thread to the term thread when all the sockets
+            //  are successfully deallocated.
+            struct {
+            } done;
+
+        } args;
+
+
+6.
---
+++ 基础组件（必须非常熟悉）.hpp
@@ -0,0 +1,36 @@
+/*	
+ *------ data structure ----------
+ * 1.  skiplist
+ * 2.  hash_table
+ * 3.  binary_tree
+ * 4.  avl_tree
+ * 5.  binary_search_tree
+ * 6.  heap(min or max)
+ * 7.  array(search data in O(1), from zeroMQ)
+ * 8.  deque
+ * 9.  rb_tree
+ * 10. ref_count data structure
+ * 11. map_reduce
+ * 12. bloom_filter
+ * 13. zset(from redis use hash_table(for O(1) find) with skiplist(for sequence))
+ * 14. varint(from redis)
+ * 15. iterator(items & cur_index)  ---->> 才有接口与实现分离的策略
+ * 16. LRU cache
+ * 17.
+ *
+ *
+ *
+ *------ algrithmn ----------
+ * 1.  贪心
+ * 2.  图算法
+ * 3.  树的相关算法
+ * 4.  动态规划
+ * 5.  排序算法
+ * 6.  搜索算法 
+ * 7.  
+ *
+ *
+ *
+ *
+ *
+ * /
